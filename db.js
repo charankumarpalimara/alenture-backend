c6aa7c1115b23dbd6e707d8f62c8a2020358607a
Palimara Charan <palimaracharan@Palimaras-MacBook-Air.local>
2025-06-09 21:05:40 +0530

    database credentials changed

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const mySqlpool = mysql.createPool({
-  host: "162.243.167.71",
-  user: "charan",
-  password: "charan@123",
-  database: "myappdb",
+  host: "161.35.54.196",  // Keep your actual host/IP
+  user: "charan",   // Replace with actual MySQL user
+  password: "Charan@786!",  // Replace with actual password
+  database: "yukthitech",
 });
-
 
 module.exports = mySqlpool;

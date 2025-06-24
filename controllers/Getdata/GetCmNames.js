665cf989cf170326e8550a5fb03127e1cc18c8aa
charankumarpalimara <133764126+charankumarpalimara@users.noreply.github.com>
2025-06-24 07:03:38 +0530

    Update GetCmNames.js

--- controllers/Getdata/GetCmNames.js
+++ controllers/Getdata/GetCmNames.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mySqlpool = require('../../db');
 
@@ -10,13 +9,16 @@ const GetCmNames = async (req, res) => {
         if (!orgName || !branch) {
             return res.status(400).json({ error: "Organization name and branch are required" });
         }
-        const [data] = await mySqlpool.query("SELECT firstname, lastname FROM listofcm WHERE organizationname = ? AND branch = ? ", [orgName,branch]);
+        const [data] = await mySqlpool.query("SELECT firstname, lastname, cmid FROM listofcm WHERE organizationname = ? AND branch = ? ", [orgName,branch]);
         if (!data || data.length === 0) {
             return res.status(404).json({ error: "No CM names found" });
         }
 
         // Map each record to a full name
-        const fullNames = data.map(cm => `${cm.firstname} ${cm.lastname}`);
+       const fullNames = data.map(cm => ({
+        name: `${cm.firstname} ${cm.lastname}`,
+        cmid: cm.cmid
+        }));
 
         res.status(200).json({ message: "CM Names Retrieved Successfully", data: fullNames });
         console.log("CM Names retrieved successfully:", fullNames);

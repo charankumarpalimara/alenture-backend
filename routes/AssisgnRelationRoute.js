541981aff07c709e2fd0c1194c4a1fe4dba2fca0
charankumarpalimara <133764126+charankumarpalimara@users.noreply.github.com>
2025-06-24 18:58:07 +0530

    Update AssisgnRelationRoute.js

--- routes/AssisgnRelationRoute.js
+++ routes/AssisgnRelationRoute.js
@@ -1,1 +1,18 @@
 
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+
+const { RelationCreate } = require('../controllers/Relations/create/Relation');
+
+
+const router = express.Router();
+
+const upload = multer();
+
+
+router.post('/createRelation', upload.none(), RelationCreate);
+
+
+module.exports = router;

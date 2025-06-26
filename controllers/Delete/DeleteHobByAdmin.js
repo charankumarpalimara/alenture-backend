f9ff36bd6603eb57d8766bfd0f30bb4f9562cfc4
charankumarpalimara <charanpalimara@gmail.com>
2025-06-26 11:41:08 +0530

    delete operation added

---
+++ controllers/Delete/DeleteHobByAdmin.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mySqlpool = require('../../db');
+
+
+const deleteHobByAdmin = async (req, res) => {
+    try {
+        const { hobid } = req.body;
+        if (!hobid) {
+            return res.status(400).json({ error: "Hob ID is required" });
+        }
+
+        // Delete the experience
+        await mySqlpool.query(
+            "DELETE FROM listofhob WHERE hob = ?",
+            [hobid]
+        );
+
+        res.status(200).json({ message: "Hob deleted successfully" });
+        console.log("Hob deleted successfully for ID:", hobid);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+
+module.exports = { deleteHobByAdmin };

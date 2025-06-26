37a266efa2dca37dc7e6b1da59fc31163e263863
charankumarpalimara <charanpalimara@gmail.com>
2025-06-26 20:11:11 +0530

    rseet password issue resolved

--- controllers/PasswordReset/cm-reset-password.js
+++ controllers/PasswordReset/cm-reset-password.js
@@ -6,21 +6,23 @@ const cmPasswordReset = async (req, res) => {
     try {
         // const noteId = req.params.id;
         const {  cmid, password } = req.body;
+        console.log("Received data:", req.body);
 
         if (!cmid || !password ) {
             return res.status(400).json({ error: "cmid ID, title, and content are required" });
         }
 
         // Check if the note exists
-        const [rm] = await mySqlpool.query("SELECT * FROM listofcm WHERE cmid = ?", [cmid]);
+        const [cm] = await mySqlpool.query("SELECT * FROM listofcm WHERE cmid = ?", [cmid]);
         if (!cm || cm.length === 0) {
             return res.status(404).json({ error: "Cm not found" });
         }
 
         // Update the note
-        await mySqlpool.query("UPDATE listofcm SET  passwords = ? WHERE cmid = ?", [cmid, password]);
+        await mySqlpool.query("UPDATE listofcm SET  passwords = ? WHERE cmid = ?", [password, cmid]);
 
         res.status(200).json({ message: "Cm updated successfully" });
+        console.log("Cm updated successfully");
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal server error" });

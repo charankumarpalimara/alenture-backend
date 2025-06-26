2cbd00ed2a9f82ba39bf0f5e979ff451c8e27d1d
charankumarpalimara <charanpalimara@gmail.com>
2025-06-26 11:41:08 +0530

    delete operation added

---
+++ controllers/Delete/DeleteCmByAdminAndHob.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mySqlpool = require('../../db');
+
+
+const deleteCmByAdminAndHob = async (req, res) => {
+    try {
+        const { cmid } = req.body;
+        if (!cmid) {
+            return res.status(400).json({ error: "Cm ID is required" });
+        }
+
+        // Delete the experience
+        await mySqlpool.query(
+            "DELETE FROM listofcm WHERE cmid = ?",
+            [cmid]
+        );
+
+        res.status(200).json({ message: "Cm deleted successfully" });
+        console.log("Cm deleted successfully for ID:", cmid);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+
+module.exports = { deleteCmByAdminAndHob };

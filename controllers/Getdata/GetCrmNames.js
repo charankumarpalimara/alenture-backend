9163a5f54931c77bcb42b232c93d3e3519dcf762
charankumarpalimara <133764126+charankumarpalimara@users.noreply.github.com>
2025-07-02 11:38:36 +0530

    Update GetCrmNames.js

--- controllers/Getdata/GetCrmNames.js
+++ controllers/Getdata/GetCrmNames.js
@@ -23,6 +23,28 @@ const GetCrmNames = async (req, res) => {
 }
 
 
-module.exports = { GetCrmNames };
+const getCrmNamesByExperienceid = async (req, res) => {
+    try {
+    const ExperienceId = req.params.experienceid;
+        const [data] = await mySqlpool.query("SELECT extraind1, extraind2 FROM experiences WHERE experienceid =  ? ORDER BY id DESC", [ExperienceId]);
+        if (!data || data.length === 0) {
+            return res.status(400).json({ error: "No Experience Not found" });
+        }
+
+        // Map each record to a full name
+        const fullNames = data.map(crm => ({
+        crmnamebyexp: `${crm.extraind2}`,
+        crmid: crm.crmid
+        }));
+        res.status(200).json({ message: "CRM Names Retrieved Successfully", data: fullNames });
+        console.log("CRM Names retrieved successfully:", fullNames);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error in Get CRM Names API" });
+    }
+}
+
+
+module.exports = { GetCrmNames, getCrmNamesByExperienceid };
 
 
